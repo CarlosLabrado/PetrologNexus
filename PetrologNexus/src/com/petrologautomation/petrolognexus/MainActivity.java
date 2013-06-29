b6924998038cceef5b2d023f7046169c4386ca9e
Cesar <cesar.chavez@petrologautomation.com>
2013-06-29 16:43:46 -0500

    Added buttons of Actions Bar

--- PetrologNexus/src/com/petrologautomation/petrolognexus/MainActivity.java
+++ PetrologNexus/src/com/petrologautomation/petrolognexus/MainActivity.java
@@ -1,18 +1,26 @@
 package com.petrologautomation.petrolognexus;
 
+import android.app.ActionBar;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
 
 
 public class MainActivity extends Activity {
-
+    ActionBar bar;
 
 
     @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+        bar = getActionBar();
+        bar.setSubtitle("");
 	}
 
 	@Override
@@ -21,5 +29,26 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.connected:
 
+                // app icon in action bar clicked; go home
+
+                bar = getActionBar();
+
+                bar.setSubtitle("Well Name");
+                return true;
+            case R.id.disconnected:
+
+                // app icon in action bar clicked; go home
+                bar = getActionBar();
+                bar.setSubtitle("");
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
