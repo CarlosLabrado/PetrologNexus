7ce0d076ece5705afa83ce9db020cd19fb8b2385
Cesar <cesar.chavez@petrologautomation.com>
2013-06-29 18:12:11 -0500

    Fixed error introduced during merge

--- PetrologNexus/src/com/petrologautomation/petrolognexus/MainActivity.java
+++ PetrologNexus/src/com/petrologautomation/petrolognexus/MainActivity.java
@@ -1,19 +1,17 @@
 package com.petrologautomation.petrolognexus;
 
 import android.app.ActionBar;
-import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Set;
 
 public class MainActivity extends Activity {
